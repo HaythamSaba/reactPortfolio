20ccf7016d9ca21b24c32f2ce26ebe62c9579a03
Haytham <104620852+HaythamSaba@users.noreply.github.com>
2026-02-19 00:34:05 +0100

    Adding a scroll up button

--- src/components/ScrollUpButton.jsx
+++ src/components/ScrollUpButton.jsx
@@ -29,4 +29,39 @@ function ScrollUpButton() {
   );
 }
 
+<<<<<<< Updated upstream
 export default ScrollUpButton;
+=======
+export default ScrollUpButton
+import { ArrowUp } from "lucide-react";
+import { useEffect, useState } from "react";
+
+function ScrollUpButton() {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      setVisible(window.scrollY > 200);
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  if (!visible) return null;
+
+  return (
+    <div
+      className="fixed bottom-4 right-4 z-50 bg-primary-300 p-2 rounded-full text-black cursor-pointer transition-all duration-300 hover:bg-primary-500 hover:text-white"
+      onClick={handleScrollToTop}
+    >
+      <ArrowUp />
+    </div>
+  );
+}
+
+>>>>>>> Stashed changes
