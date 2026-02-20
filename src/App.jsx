836025397e8cc13381281bf8e9c3554f5d2a315b
Haytham <104620852+HaythamSaba@users.noreply.github.com>
2026-02-20 14:33:57 +0100

    add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Lenis as ReactLenis } from "@studio-freight/react-lenis";
-
+import { Analytics } from "@vercel/analytics/react";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <BrowserRouter>
-      <ReactLenis root>
+    <ReactLenis root>
+      <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </ReactLenis>
-    </BrowserRouter>
+      </BrowserRouter>
+      <Analytics />
+    </ReactLenis>
   );
 }
+
 export default App;
