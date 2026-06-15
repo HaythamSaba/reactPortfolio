431dbaba9728edb9e4a222264df54e7143e507c8
Haytham <104620852+HaythamSaba@users.noreply.github.com>
2026-06-15 19:10:15 +0200

    refactoring certificate component, creating certificate card

--- src/components/sections/Certificates.jsx
+++ src/components/sections/Certificates.jsx
@@ -2,19 +2,11 @@ import { useState, useEffect, useRef } from "react";
 import { motion, useAnimation, useInView } from "framer-motion";
 import { certificates } from "../../data/StaticData";
 import CertificateModal from "../ui/CertificateModal";
+import CertificateCard from "../ui/CertificateCard";
 
 const containerVariants = {
   hidden: {},
   visible: { transition: { staggerChildren: 0.2 } },
-};
-
-const itemVariants = {
-  hidden: { opacity: 0, y: 40 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: { duration: 0.7, ease: "easeOut" },
-  },  
 };
 
 function Certificates() {
@@ -71,38 +63,13 @@ function Certificates() {
         animate={mainControls}
       >
         {certificates.map((certificate, index) => (
-          <motion.div
-            key={certificate.name}
-            variants={itemVariants}
-            className="bg-background rounded-lg overflow-hidden shadow-md hover:shadow-xl transition-shadow"
-          >
-            {/* Image preview */}
-            <div className="relative w-full h-64 flex items-center justify-center bg-slate-100">
-              <img
-                src={certificate.imageUrl}
-                alt={`Preview of ${certificate.name}`}
-                className="max-h-full max-w-full object-contain rounded-t-lg"
-              />
-            </div>
-
-            <div className="p-4 flex flex-col justify-between">
-              <div>
-                <h3 className="text-lg font-semibold text-slate-900 mb-2">
-                  {certificate.name}
-                </h3>
-                <p className="text-sm text-slate-800 mb-4">
-                  {certificate.description}
-                </p>
-              </div>
-              <button
-                className="bg-primary-500 hover:bg-primary-600 font-semibold text-textColor py-2 px-4 rounded-full transition-colors"
-                onClick={() => setOpenIndex(index)}
-                aria-label={`View ${certificate.name}`}
-              >
-                View Certificate
-              </button>
-            </div>
-          </motion.div>
+          <CertificateCard
+            key={index}
+            index={index}
+            certificate={certificate}
+            openIndex={openIndex}
+            setOpenIndex={setOpenIndex}
+          />
         ))}
       </motion.div>
 
---
+++ src/components/ui/CertificateCard.jsx
@@ -0,0 +1,51 @@
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 40 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.7, ease: "easeOut" },
+  },
+};
+
+function CertificateCard({ certificate, index, openIndex, setOpenIndex }) {
+  
+  return (
+    <motion.div
+      key={certificate.name}
+      variants={itemVariants}
+      className="bg-background rounded-lg overflow-hidden shadow-md hover:shadow-xl transition-shadow"
+    >
+      {/* Image preview */}
+      <div className="relative w-full h-64 flex items-center justify-center bg-slate-100">
+        <img
+          src={certificate.imageUrl}
+          alt={`Preview of ${certificate.name}`}
+          className="max-h-full max-w-full object-contain rounded-t-lg"
+        />
+      </div>
+
+      <div className="p-4 flex flex-col justify-between">
+        <div>
+          <h3 className="text-lg font-semibold text-slate-900 mb-2">
+            {certificate.name}
+          </h3>
+          <p className="text-sm text-slate-800 mb-4">
+            {certificate.description}
+          </p>
+        </div>
+        <button
+          className="bg-primary-500 hover:bg-primary-600 font-semibold text-textColor py-2 px-4 rounded-full transition-colors"
+          onClick={() => setOpenIndex(index)}
+          aria-label={`View ${certificate.name}`}
+        >
+          View Certificate
+        </button>
+      </div>
+    </motion.div>
+  );
+}
+
+export default CertificateCard;
