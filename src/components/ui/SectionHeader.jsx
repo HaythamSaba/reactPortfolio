d6ace7855a872448f668e8167ee9ffc02a98f262
Haytham <104620852+HaythamSaba@users.noreply.github.com>
2026-08-30 21:38:25 +0200

    adding blur header

--- src/components/ui/SectionHeader.jsx
+++ src/components/ui/SectionHeader.jsx
@@ -11,10 +11,16 @@ function SectionHeader({
       className="flex flex-col gap-4 mb-12 text-center items-center"
       initial={{ opacity: 0, y: -110 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, margin: "-200px" }}
+      viewport={{ once: true, margin: "-160px" }}
       transition={{ duration: 0.8, ease: "easeOut" }}
     >
-      <h2 className="text-3xl md:text-7xl font-extrabold tracking-wide text-slate-100">
+      <motion.h2
+        className="text-3xl md:text-7xl font-extrabold tracking-wide text-slate-100"
+        initial={{ filter: "blur(20px)" }}
+        whileInView={{ filter: "blur(0px)" }}
+        viewport={{ once: true, margin: "-160px" }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+      >
         {title}{" "}
         {firstEmphasisTitle && (
           <span className="text-primary-400">{firstEmphasisTitle}</span>
@@ -22,7 +28,7 @@ function SectionHeader({
         {secondEmphasisTitle && (
           <span className="text-secondary-400">{secondEmphasisTitle}</span>
         )}
-      </h2>
+      </motion.h2>
       {description && (
         <p className="text-slate-300 max-w-xl">{description}</p>
       )}{" "}
